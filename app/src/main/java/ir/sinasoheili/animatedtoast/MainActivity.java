27d5aabf765f79896e08b1eff1e1437c242cc4ee
SINA <sinasoheili79@gmail.com>
2019-12-31 13:09:19 +0330

    create animated toast and show it in activity

--- app/src/main/java/ir/sinasoheili/animatedtoast/MainActivity.java
+++ app/src/main/java/ir/sinasoheili/animatedtoast/MainActivity.java
@@ -3,6 +3,9 @@ package ir.sinasoheili.animatedtoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+
+import ir.sinasoheili.animated_toast.AnimatedToast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create an animated toast and show that
+        AnimatedToast t = new AnimatedToast(this , "Animated Toast" , R.drawable.home);
+        t.setGravity(Gravity.CENTER_VERTICAL , 0 , 0 );
+        t.show();
     }
 }
